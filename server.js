344cd069d5c12ee8e933e974ac21a765c6fbabfa
Soni Kumari <thissoni0909@gmail.com>
2026-09-12 15:49:15 +0530

    listing route setUp

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const mongoose = require('mongoose');
 // requiring model "Listing"
 const { Listing } = require('./models/listing');
 
+// requiring path
+const path = require('path');
+
+// set template engine "ejs"
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+// serving static files
+app.use(express.static(path.join(__dirname,"/public")));
+
 // establishing connection between mongoDB database
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/StayNest');
@@ -34,6 +44,19 @@ app.get("/testing",(req, res) => {
         console.log(err);
     });
     res.send("Successfully Saved");
+});
+
+// home route
+app.get("/listings", async (req, res) => {
+    const AllListings = await Listing.find();
+    res.render("Listings/home.ejs", { AllListings });
+});
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("Listings/show.ejs", { listing });
 });
 
 // starting express server
